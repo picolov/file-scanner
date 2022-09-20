6842d1ed3779de36b530558038ea3b6f6bb8392a
Charel Samuel <charel.matulessy@aux.dkatalis.com>
2022-09-20 13:35:33 +0700

    add scan file function

--- src/scanner.js
+++ src/scanner.js
@@ -1,1 +1,13 @@
-console.log("scanner");
+const path = require("path");
+const fs = require("fs");
+
+const scan = (filepath) => {
+  const testFolder = path.join(__dirname, filepath);
+  const filenames = fs.readdirSync(testFolder);
+
+  return filenames;
+};
+
+module.exports = {
+  scan,
+};
--- src/scanner.test.js
+++ src/scanner.test.js
@@ -1,5 +1,17 @@
 const scanner = require("./scanner");
 
-test("Scan folder content", () => {
-  expect(scanner.scan().toBe(["dummy.txt"]));
+test("Scan folder containing 0 file", () => {
+  expect(JSON.stringify(scanner.scan("../dummy/test0"))).toStrictEqual(
+    JSON.stringify([])
+  );
+});
+
+test("Scan folder containing 1 file", () => {
+  expect(JSON.stringify(scanner.scan("../dummy/test1"))).toStrictEqual(
+    JSON.stringify(["dummy.txt"])
+  );
+});
+
+test("Scan folder containing 100 files", () => {
+  expect(scanner.scan("../dummy/test2").length).toBe(100);
 });
